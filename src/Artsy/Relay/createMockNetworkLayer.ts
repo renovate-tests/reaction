7dd250a904da793601f2435221778915855e15ee
Orta Therox <orta.therox@gmail.com>
2018-08-31 12:23:51 -0400

    WIP removes the schema json:

--- src/Artsy/Relay/createMockNetworkLayer.ts
+++ src/Artsy/Relay/createMockNetworkLayer.ts
@@ -1,9 +1,19 @@
+import { readFileSync } from "fs"
+import { makeExecutableSchema } from "graphql-tools"
 import { IMocks } from "graphql-tools/dist/Interfaces"
 import getNetworkLayer from "relay-mock-network-layer"
 import { Network } from "relay-runtime"
-import schema from "../../../data/schema.json"
+
+let schema: any | undefined
 
 export const createMockNetworkLayer = (mockResolvers: IMocks) => {
+  if (!schema) {
+    const metaphysicsSchema = readFileSync(
+      "../data/metaphysics.graphql",
+      "utf8"
+    )
+    schema = makeExecutableSchema({ typeDefs: metaphysicsSchema })
+  }
   return Network.create(
     getNetworkLayer({
       schema,
