3b537320fcf5faaae007ffb6b5f8dbff7f88bbab
David Sheldrick <djsheldrick@gmail.com>
2019-04-17 10:43:11 +0100

    add solution for useJson

--- src/Apps/Artist/Components/ArtistDetails.tsx
+++ src/Apps/Artist/Components/ArtistDetails.tsx
@@ -1,5 +1,5 @@
 import { Flex, Sans, Spinner } from "@artsy/palette"
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 export function ArtistDetails({ id }: { id: string }) {
   const [artist, { loading, error }] = useJson(
@@ -34,12 +34,50 @@ export function ArtistDetails({ id }: { id: string }) {
   )
 }
 
-function useJson(
-  url: string
-): [any, { loading: boolean; error: Error | null }] {
-  // TODO: implement me
-  return [
-    { name: "David", hometown: "Brighton, United Kingdom" },
-    { loading: false, error: null },
-  ]
+function useJson(url: string): [any, { loading: boolean; error: Error }] {
+  const [data, setData] = useState(undefined)
+  const [error, setError] = useState<Error | null>(null)
+  const abortController = useRef<AbortController>(null)
+
+  useEffect(() => {
+    // reset data+error to 'undefined' to show the loading spinner
+    setData(undefined)
+    setError(null)
+    // If there is an in-flight request already, abort it.
+    if (abortController.current) {
+      abortController.current.abort()
+    }
+
+    const controller = new AbortController()
+
+    // make a new abortable request
+    abortController.current = controller
+    fetch(url, {
+      signal: controller.signal,
+      headers: {
+        // make sure to replace <YOUR_ACCESS_TOKEN> by logging in to staging,
+        // then running `copy(sd.CURRENT_USER.accessToken)` in the dev tools
+        // console, and then pasting it in here.
+        "X-Xapp-Token": "<YOUR_ACCESS_TOKEN>",
+      },
+    })
+      .then(response => {
+        if (response.status >= 300) {
+          throw new Error("Got HTTP " + response.status)
+        }
+        return response.json()
+      })
+      .then(setData)
+      .then(() => setError(null))
+      .catch(e => {
+        // note that aborted requests manifest as errors here, so we need to
+        // check that this request was not aborted
+        if (abortController.current !== controller) {
+          return
+        }
+        setError(e)
+      })
+  }, [url])
+
+  return [data, { loading: data === undefined && !error, error }]
 }
