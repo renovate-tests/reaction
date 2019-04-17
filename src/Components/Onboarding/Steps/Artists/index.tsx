7d38b078fd138400cb8053bbb06a8c126bd96119
David Sheldrick <djsheldrick@gmail.com>
2019-04-17 14:01:10 +0100

    refactor Artists to use hooks

--- src/Components/Onboarding/Steps/Artists/index.tsx
+++ src/Components/Onboarding/Steps/Artists/index.tsx
@@ -1,5 +1,5 @@
 import { throttle } from "lodash"
-import React from "react"
+import React, { useCallback, useState } from "react"
 import styled from "styled-components"
 
 import Colors from "../../../../Assets/Colors"
@@ -21,83 +21,53 @@ const OnboardingSearchBox = styled.div`
   `};
 `
 
-interface State {
-  inputText: string
-  followCount: number
-  inputTextQuery: string
+export default function Artists(props: StepProps) {
+  const [inputTextQuery, setInputTextQuery] = useState("")
+  const [followCount, setFollowCount] = useState(0)
+
+  const updateFollowCount = useCallback(
+    (count: number) => setFollowCount(followCount + count),
+    [followCount, setFollowCount]
+  )
+
+  const throttledTextChange = useCallback(
+    throttle(e => setInputTextQuery(e.target.value)),
+    [setInputTextQuery]
+  )
+
+  const submit = useCallback(() => {
+    const increaseBy = followCount >= 4 ? 2 : 1
+    props.onNextButtonPressed(increaseBy)
+  }, [followCount])
+
+  return (
+    <Layout
+      title="Who are your favorite artists?"
+      subtitle="Follow one or more"
+      onNextButtonPressed={submit}
+      buttonState={
+        followCount > 0
+          ? MultiButtonState.Highlighted
+          : MultiButtonState.Default
+      }
+    >
+      <OnboardingSearchBox>
+        <Input
+          placeholder={"Search artists..."}
+          block
+          onInput={throttledTextChange}
+          onPaste={throttledTextChange}
+          onCut={throttledTextChange}
+          autoFocus
+        />
+        <div style={{ marginBottom: "35px" }} />
+        <ArtistList
+          searchQuery={inputTextQuery}
+          updateFollowCount={updateFollowCount}
+        />
+      </OnboardingSearchBox>
+    </Layout>
+  )
 }
 
-export default class Artists extends React.Component<StepProps, State> {
-  static slug: "artists" = "artists"
-
-  state = {
-    inputText: "",
-    inputTextQuery: "",
-    followCount: 0,
-  }
-
-  updateFollowCount(count: number) {
-    const updatedFollowCount = this.state.followCount + count
-
-    this.setState({ followCount: updatedFollowCount })
-  }
-
-  submit() {
-    const increaseBy = this.state.followCount >= 4 ? 2 : 1
-    this.props.onNextButtonPressed(increaseBy)
-  }
-
-  componentDidMount() {
-    this.throttledTextChange = throttle(this.throttledTextChange, 500, {
-      leading: true,
-    })
-  }
-
-  searchTextChanged = e => {
-    const updatedInputText = e.target.value
-    this.setState({ inputText: updatedInputText })
-    this.throttledTextChange(updatedInputText)
-  }
-
-  throttledTextChange = inputText => {
-    this.setState({ inputTextQuery: inputText })
-  }
-
-  clearSearch(e) {
-    this.setState({
-      inputText: "",
-      inputTextQuery: "",
-    })
-  }
-
-  render() {
-    return (
-      <Layout
-        title="Who are your favorite artists?"
-        subtitle="Follow one or more"
-        onNextButtonPressed={this.submit.bind(this)}
-        buttonState={
-          this.state.followCount > 0
-            ? MultiButtonState.Highlighted
-            : MultiButtonState.Default
-        }
-      >
-        <OnboardingSearchBox>
-          <Input
-            placeholder={"Search artists..."}
-            block
-            onInput={this.searchTextChanged.bind(this)}
-            onPaste={this.searchTextChanged.bind(this)}
-            onCut={this.searchTextChanged.bind(this)}
-            autoFocus
-          />
-          <div style={{ marginBottom: "35px" }} />
-          <ArtistList
-            searchQuery={this.state.inputTextQuery}
-            updateFollowCount={this.updateFollowCount.bind(this)}
-          />
-        </OnboardingSearchBox>
-      </Layout>
-    )
-  }
-}
+Artists.slug = "artists"
