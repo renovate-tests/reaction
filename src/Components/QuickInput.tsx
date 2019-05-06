689f3002aaad9783cbcd6e4b65ab6e0741315010
David Sheldrick <d.j.sheldrick@gmail.com>
2019-05-06 11:04:06 +0100

    Refactor QuickInput to use hooks

--- src/Components/QuickInput.tsx
+++ src/Components/QuickInput.tsx
@@ -2,7 +2,7 @@ import { space } from "@artsy/palette"
 import { fadeIn, fadeOut } from "Assets/Animations"
 import Colors from "Assets/Colors"
 import { garamond, unica } from "Assets/Fonts"
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import { borderedInput } from "./Mixins"
 
@@ -28,97 +28,80 @@ export interface QuickInputState {
  * Quick input. Renders the label inside of the textbox.
  *
  */
-export class QuickInput extends React.Component<
-  QuickInputProps,
-  QuickInputState
-> {
-  state = {
-    focused: false,
-    value: (this.props.value as string) || "",
-    touchedOnChange: true,
+export function QuickInput(props: QuickInputProps) {
+  const [name, setName] = useState(props.name)
+  const [focused, setFocused] = useState(false)
+  const [value, setValue] = useState((props.value as string) || "")
+
+  if (props.name !== name) {
+    setName(props.name)
+    setValue("")
   }
 
-  componentWillReceiveProps(newProps) {
-    if (this.props.name !== newProps.name) {
-      this.setState({
-        value: "",
-      })
+  const onFocus = e => {
+    setFocused(true)
+
+    if (props.onFocus) {
+      props.onFocus(e)
     }
   }
 
-  onFocus = e => {
-    this.setState({
-      focused: true,
-    })
+  const onBlur = e => {
+    if (props.setTouched) {
+      props.setTouched({ [name]: true })
+    }
+    setFocused(false)
 
-    if (this.props.onFocus) {
-      this.props.onFocus(e)
+    if (props.onBlur) {
+      props.onBlur(e)
     }
   }
 
-  onBlur = e => {
-    if (this.props.setTouched) {
-      this.props.setTouched({ [this.props.name]: true })
+  const onChange = e => {
+    if (props.touchedOnChange && props.setTouched) {
+      props.setTouched({ [name]: true })
     }
-    this.setState({
-      focused: false,
-    })
+    setValue(e.currentTarget.value)
 
-    if (this.props.onBlur) {
-      this.props.onBlur(e)
+    if (props.onChange) {
+      props.onChange(e)
     }
   }
 
-  onChange = e => {
-    if (this.props.touchedOnChange && this.props.setTouched) {
-      this.props.setTouched({ [this.props.name]: true })
-    }
-    this.setState({
-      value: e.currentTarget.value,
-    })
+  const {
+    error,
+    className,
+    label,
+    ref: _ref,
+    setTouched,
+    rightAddOn,
+    note,
+    ...newProps
+  } = props
+  const showLabel = (!!focused || !!value) && !!label
 
-    if (this.props.onChange) {
-      this.props.onChange(e)
-    }
-  }
-
-  render() {
-    const {
-      error,
-      className,
-      label,
-      ref: _ref,
-      onChange,
-      setTouched,
-      rightAddOn,
-      note,
-      ...newProps
-    } = this.props
-    const showLabel = (!!this.state.focused || !!this.state.value) && !!label
-
-    return (
-      <Container>
-        <InputContainer
-          hasLabel={!!label}
-          hasError={!!error}
-          className={this.state.focused ? "focused" : ""}
-        >
-          <Label out={!showLabel}>{label}</Label>
-          <InputComponent
-            {...newProps}
-            onFocus={this.onFocus}
-            onBlur={this.onBlur}
-            onChange={this.onChange}
-            value={this.state.value}
-            showLabel={showLabel}
-          />
-          {rightAddOn}
-        </InputContainer>
-        {note && <InputNote>{note}</InputNote>}
-        {error && <InputError>{error}</InputError>}
-      </Container>
-    )
-  }
+  return (
+    <Container>
+      <InputContainer
+        hasLabel={!!label}
+        hasError={!!error}
+        className={focused ? "focused" : ""}
+      >
+        <Label out={!showLabel}>{label}</Label>
+        <InputComponent
+          {...newProps}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onChange={onChange}
+          value={value}
+          showLabel={showLabel}
+        />
+        {rightAddOn}
+      </InputContainer>
+      {note && <InputNote>{note}</InputNote>}
+      {error && <InputError>{error}</InputError>}
+    </Container>
+  )
 }
 
 const Container = styled.div`
